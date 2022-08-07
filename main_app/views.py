f941ab4f11ff3e9936978f1872f82a10e03a3dc6
ilysim <illymarie1@live.com>
2022-08-07 13:06:06 -0500

    create about template

--- main_app/views.py
+++ main_app/views.py
@@ -6,4 +6,4 @@ def home(request):
   return HttpResponse('<h1>This is the home page!!</h1>')
 
 def about(request):
-  return HttpResponse('<h1>This is the about page!</h1>')
+  return render(request, 'about.html')
