ec10dc34828959c5fa321f14e32b46542540b606
ilysim <illymarie1@live.com>
2022-08-07 14:08:28 -0500

    create finch model & finches

--- main_app/views.py
+++ main_app/views.py
@@ -1,24 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-class Finch:
-  def __init__(self, name, description, age):
-    self.name = name
-    self.description = description
-    self.age = age
-
-finches= {
-  Finch('birb', 'tiny birb', 3),
-  Finch('BIRD', 'tiny birb', 0),
-  Finch('birb', 'tiny birb', 1)
-}
+from .models import Finch
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>This is the home page!!</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def finches_index(request):
+  finches = Finch.objects.all()
   return render(request, 'finches/index.html', {'finches': finches})
