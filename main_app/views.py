87f7bf7d4f18b3cc6bc94d43681c37963142cae1
ilysim <illymarie1@live.com>
2022-08-07 15:33:31 -0500

    create update & delete views

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
+from ast import Delete
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 
 # Create your views here.
@@ -20,3 +21,11 @@ def finches_detail(request, finch_id):
 class FinchCreate(CreateView):
   model = Finch
   fields = '__all__'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = ['description', 'age']
+
+class FinchDelete(DeleteView):
+  model = Finch 
+  success_url = '/finches/'
