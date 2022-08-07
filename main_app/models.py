946809ba31e2bb414f0a45cc2df0c7a503b7ed14
ilysim <illymarie1@live.com>
2022-08-07 16:22:33 -0500

    implement feeding view

--- main_app/models.py
+++ main_app/models.py
@@ -4,6 +4,12 @@ from django.urls import reverse
 
 # Create your models here.
 
+MEALS = (
+  ('F', 'Fruit'),
+  ('S', 'Seeds'),
+  ('W', 'Worms')
+)
+
 class Finch(models.Model):
   name = models.CharField(max_length=100)
   description = models.CharField(max_length=100)
@@ -14,4 +20,18 @@ class Finch(models.Model):
   
   def get_absolute_url(self):
       return reverse("finches_detail", kwargs={"finch_id": self.id})
+
+class Feeding(models.Model):
+  date = models.DateField('Feeding date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+  finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
   
+  class Meta:
+    ordering = ['-date']
