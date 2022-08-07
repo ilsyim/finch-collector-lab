ca38b55d8c075bf2021e736bbc6df67aa200f1d7
ilysim <illymarie1@live.com>
2022-08-07 18:39:13 -0500

    set up sign up

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from main_app.forms import FeedingForm
 from .models import Finch, Toy
 
 # Create your views here.
-def home(request):
-  return render(request, 'home.html')
+class Home(LoginView):
+  template_name = 'home.html'
 
 def about(request):
   return render(request, 'about.html')
@@ -37,6 +40,10 @@ class FinchCreate(CreateView):
   model = Finch
   fields = ['name', 'description', 'age']
 
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
 class FinchUpdate(UpdateView):
   model = Finch
   fields = ['description', 'age']
@@ -66,3 +73,17 @@ class ToyDelete(DeleteView):
 def assoc_toy(request, finch_id, toy_id):
   Finch.objects.get(id=finch_id).toys.add(toy_id)
   return redirect('finches_detail', finch_id=finch_id)
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('finches_index')
+    else:
+      error_message = 'Invalid sign up - try again!'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
